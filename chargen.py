6f107b5ccd7474cce29494a7449b1d51eb00a1c0
Cyrus Malekpour <cmalekpour@gmail.com>
2015-10-17 16:41:23 -0400

    Removed some example code from servers

--- chargen.py
+++ chargen.py
@@ -17,16 +17,6 @@ class ThreadedTCPRequestHandler(SocketServer.BaseRequestHandler):
 class ThreadedTCPServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
     pass
 
-def client(ip, port, message):
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.connect((ip, port))
-    try:
-        sock.sendall(message)
-        response = sock.recv(1024)
-        print "Received: {}".format(response)
-    finally:
-        sock.close()
-
 if __name__ == "__main__":
     # Port 0 means to select an arbitrary unused port
     HOST, PORT = "0.0.0.0", 0
--- highlow.py
+++ highlow.py
@@ -27,16 +27,6 @@ class ThreadedTCPRequestHandler(SocketServer.BaseRequestHandler):
 class ThreadedTCPServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
     pass
 
-def client(ip, port, message):
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.connect((ip, port))
-    try:
-        sock.sendall(message)
-        response = sock.recv(1024)
-        print "Received: {}".format(response)
-    finally:
-        sock.close()
-
 if __name__ == "__main__":
     # Port 0 means to select an arbitrary unused port
     HOST, PORT = "0.0.0.0", 0
@@ -54,10 +44,6 @@ if __name__ == "__main__":
     server_thread.daemon = True
     server_thread.start()
     print "Server loop running in thread:", server_thread.name
-
-    client(ip, port, "Hello World 1")
-    client(ip, port, "Hello World 2")
-    client(ip, port, "Hello World 3")
 
     try:
         while True:
